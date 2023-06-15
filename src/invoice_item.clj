8f08a65379758f52bee79c27a73bc4529c96c59a
Joseph D Hernandez <jophhernandez@gmail.com>
2023-06-15 17:31:14 -0500

    feat: filter invoice based on condition (problem #1)

--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -10,3 +10,33 @@
     :as                item
     :or                {discount-rate 0}}]
   (* precise-price precise-quantity (discount-factor item)))
+
+(defn load-invoice
+  "Load the invoice file"
+  []
+  (clojure.edn/read-string (slurp "invoice.edn")))
+
+(defn valid-category?
+  [category rate category-type rate-value]
+  (cond (= category category-type) (= rate rate-value)))
+
+(defn check-iva?
+  [{:tax/keys [category rate]}]
+  (valid-category? category rate :iva 19))
+
+(defn check-ret?
+  [{:retention/keys [category rate]}]
+  (valid-category? category rate :ret_fuente 1))
+
+(defn check-invoice-item
+  [{:taxable/keys [taxes] :retentionable/keys [retentions]}]
+  (let [tax (check-iva? (first taxes)) ret (check-ret? (first retentions))]
+    (if (and tax ret) false
+        (or tax ret))))
+
+(defn validate-invoice
+  []
+  (->>
+   (load-invoice)
+   (:invoice/items)
+   (filter check-invoice-item)))
